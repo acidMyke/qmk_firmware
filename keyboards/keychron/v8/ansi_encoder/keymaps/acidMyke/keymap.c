a3285427ad84b5f8ef78070cb0a6f25e90695028
acidMyke <owner@acidmyke.link>
2024-07-27 14:35:44 +0800

    Mod Tap Layout

--- keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/keymap.c
+++ keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,             KC_LWIN, KC_LALT, /*KC_NO*/ KC_SPC, /*KC_NO*/ MO(_FN2), MO(_FN3), /*KC_NO*/ KC_SPC, /*KC_NO*/ KC_RALT, /*KC_NO*/ KC_LEFT, KC_DOWN,   KC_RGHT),
 
     [__MT] = LAYOUT_ansi_69(
-        KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(_FN2), MO(_FN3),         KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_GRV,               KC_1,               KC_2,               KC_3,               KC_4,                KC_5,         KC_6,       KC_7,       KC_8,              KC_9,              KC_0,              KC_MINS,                          KC_EQL,   KC_BSPC, /*KC_NO*/ KC_MUTE,
+        KC_TAB,               KC_Q,               KC_W,               KC_E,               KC_R,                KC_T,         KC_Y,       KC_U,       KC_I,              KC_O,              KC_P,              KC_LBRC,                          KC_RBRC,  KC_BSLS, /*KC_NO*/ KC_PSCR,
+        MT(MOD_LCTL, KC_ESC), MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F),  LT(2,KC_G),   /*KC_NO*/   LT(2,KC_H), MT(MOD_LCTL,KC_J), MT(MOD_LSFT,KC_K), MT(MOD_LALT,KC_L), MT(MOD_LGUI | MOD_RGUI,KC_SCLN),  KC_QUOT,  KC_ENT,  /*KC_NO*/ KC_DEL,
+        KC_LSFT,                                  KC_Z,               KC_X,               LT(2,KC_C),          LT(3,KC_V),   LT(4,KC_B), LT(4,KC_B), LT(3,KC_N),        LT(2,KC_M),        KC_COMM,           KC_DOT,                           KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCTL,              KC_LWIN,            KC_LALT,            /*KC_NO*/           KC_SPC,              /*KC_NO*/     MO(_FN2),   MO(_FN3),   /*KC_NO*/          KC_SPC,            /*KC_NO*/          KC_RALT,                          /*KC_NO*/ KC_LEFT, KC_DOWN,  KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_69(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_NO,   KC_NO,   RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
