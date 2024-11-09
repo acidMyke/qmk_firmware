469cb5dc1dd32d4e5d2e3e8fb6600002ecf35e90
acidMyke <owner@acidmyke.link>
2024-11-09 09:40:15 +0800

    disable double shift for caps

--- keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/keymap.c
+++ keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/keymap.c
@@ -20,12 +20,10 @@
 
 // Tap dance definitions
 enum {
-    TD_SHIFT_CAPS,
     TD_GRV_TILD,
 };
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
     [TD_GRV_TILD] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_TILD),
 };
 
@@ -122,11 +120,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,         KC_LWIN, KC_LALT,       KC_SPC,       MO(_FN1), MO(_FN2),       KC_SPC,          KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT),
     // Home row mods enabled
     [__MT] = LAYOUT_ansi_69(
-        TD(TD_GRV_TILD),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,          KC_RBRC,  KC_BSLS,          KC_PSCR,
-        MT_ESC,            MT_A,    MT_S,    MT_D,    MT_F,    MT_G,             MT_H,    MT_J,    MT_K,    MT_L,    MT_SCLN, KC_QUOT, KC_ENT,                     KC_DEL,
-        TD(TD_SHIFT_CAPS),          KC_Z,    KC_X,    LT_C,    LT_V,    LT_B,    LT_B,    LT_N,    LT_M,    KC_COMM, KC_DOT,           KC_SLSH,  KC_RSFT, KC_UP,
-        _______,           _______, _______,          _______,          _______, _______,          _______,          _______,                    KC_LEFT, KC_DOWN, KC_RGHT),
+        TD(TD_GRV_TILD), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,         KC_MUTE,
+        KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_PSCR,
+        MT_ESC,          MT_A,    MT_S,    MT_D,    MT_F,    MT_G,             MT_H,    MT_J,    MT_K,    MT_L,    MT_SCLN, KC_QUOT, KC_ENT,          KC_DEL,
+        KC_LSFT,         KC_Z,    KC_X,    LT_C,    LT_V,    LT_B,    LT_B,    LT_N,    LT_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
+        _______,         _______, _______,          _______,          _______, _______,          _______,          _______, KC_LEFT, KC_DOWN, KC_RGHT),
     // Function layer 1 (Home row numbers + F1-12 keys)
     [_FN1] = LAYOUT_ansi_69(
         KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          KC_MPLY,
