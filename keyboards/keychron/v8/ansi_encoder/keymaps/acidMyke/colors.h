2eba16784ef7c60aa8132f28656a27d76567f8a9
acidMyke <owner@acidmyke.link>
2025-06-03 22:19:48 +0800

    refactor: move colors into its file

---
+++ keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/colors.h
@@ -0,0 +1,38 @@
+// #define DIM_RGB_AZURE       153-80, 245-80, 255-80
+// #define DIM_RGB_BLUE        0, 0, 255-80
+// #define DIM_RGB_CHARTREUSE  128-80, 255-80, 0
+// #define DIM_RGB_CORAL       255-80, 124-80, 77-80
+// #define DIM_RGB_CYAN        0, 255-80, 255-80
+// #define DIM_RGB_GOLD        255-80, 217-80, 0
+// #define DIM_RGB_GOLDENROD   217-80, 165-80, 33-80
+// #define DIM_RGB_GREEN       0, 255-80, 0
+// #define DIM_RGB_MAGENTA     255-80, 0, 255-80
+// #define DIM_RGB_ORANGE      255-80, 128-80, 0
+// #define DIM_RGB_PINK        255-80, 128-80, 191-80
+// #define DIM_RGB_PURPLE      122-80, 0, 255-80
+// #define DIM_RGB_RED         255-80, 0, 0
+// #define DIM_RGB_SPRINGGREEN 0, 255-80, 128-80
+// #define DIM_RGB_TEAL        0, 128-80, 128-80
+// #define DIM_RGB_TURQUOISE   71-80, 110-80, 106-80
+// #define DIM_RGB_WHITE       255-80, 255-80, 255-80
+// #define DIM_RGB_YELLOW      255-80, 255-80, 0
+
+#define DIM_RGB_AZURE       73, 165, 175
+#define DIM_RGB_BLACK       0, 0, 0
+#define DIM_RGB_BLUE        0, 0, 175
+#define DIM_RGB_CHARTREUSE  48, 175, 0
+#define DIM_RGB_CORAL       175, 44, 0
+#define DIM_RGB_CYAN        0, 175, 175
+#define DIM_RGB_GOLD        175, 137, 0
+#define DIM_RGB_GOLDENROD   137, 85, 0
+#define DIM_RGB_GREEN       0, 175, 0
+#define DIM_RGB_MAGENTA     175, 0, 175
+#define DIM_RGB_ORANGE      175, 48, 0
+#define DIM_RGB_PINK        175, 38, 95
+#define DIM_RGB_PURPLE      42, 0, 175
+#define DIM_RGB_RED         175, 0, 0
+#define DIM_RGB_SPRINGGREEN 0, 175, 48
+#define DIM_RGB_TEAL        0, 48, 48
+#define DIM_RGB_TURQUOISE   0, 30, 26
+#define DIM_RGB_WHITE       175, 175, 175
+#define DIM_RGB_YELLOW      175, 175, 0
--- keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/keymap.c
+++ keyboards/keychron/v8/ansi_encoder/keymaps/acidMyke/keymap.c
@@ -15,6 +15,7 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "./colors.h"
 #include "./tapdance.h"
 #include "./password.h"
 // clang-format off
@@ -187,45 +188,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_FN3]   = {ENCODER_CCW_CW(RM_VALD, RM_VALU)}
 };
 #endif // ENCODER_MAP_ENABLE
-
-// #define DIM_RGB_AZURE       153-80, 245-80, 255-80
-// #define DIM_RGB_BLUE        0, 0, 255-80
-// #define DIM_RGB_CHARTREUSE  128-80, 255-80, 0
-// #define DIM_RGB_CORAL       255-80, 124-80, 77-80
-// #define DIM_RGB_CYAN        0, 255-80, 255-80
-// #define DIM_RGB_GOLD        255-80, 217-80, 0
-// #define DIM_RGB_GOLDENROD   217-80, 165-80, 33-80
-// #define DIM_RGB_GREEN       0, 255-80, 0
-// #define DIM_RGB_MAGENTA     255-80, 0, 255-80
-// #define DIM_RGB_ORANGE      255-80, 128-80, 0
-// #define DIM_RGB_PINK        255-80, 128-80, 191-80
-// #define DIM_RGB_PURPLE      122-80, 0, 255-80
-// #define DIM_RGB_RED         255-80, 0, 0
-// #define DIM_RGB_SPRINGGREEN 0, 255-80, 128-80
-// #define DIM_RGB_TEAL        0, 128-80, 128-80
-// #define DIM_RGB_TURQUOISE   71-80, 110-80, 106-80
-// #define DIM_RGB_WHITE       255-80, 255-80, 255-80
-// #define DIM_RGB_YELLOW      255-80, 255-80, 0
-
-#define DIM_RGB_AZURE       73, 165, 175
-#define DIM_RGB_BLACK       0, 0, 0
-#define DIM_RGB_BLUE        0, 0, 175
-#define DIM_RGB_CHARTREUSE  48, 175, 0
-#define DIM_RGB_CORAL       175, 44, 0
-#define DIM_RGB_CYAN        0, 175, 175
-#define DIM_RGB_GOLD        175, 137, 0
-#define DIM_RGB_GOLDENROD   137, 85, 0
-#define DIM_RGB_GREEN       0, 175, 0
-#define DIM_RGB_MAGENTA     175, 0, 175
-#define DIM_RGB_ORANGE      175, 48, 0
-#define DIM_RGB_PINK        175, 38, 95
-#define DIM_RGB_PURPLE      42, 0, 175
-#define DIM_RGB_RED         175, 0, 0
-#define DIM_RGB_SPRINGGREEN 0, 175, 48
-#define DIM_RGB_TEAL        0, 48, 48
-#define DIM_RGB_TURQUOISE   0, 30, 26
-#define DIM_RGB_WHITE       175, 175, 175
-#define DIM_RGB_YELLOW      175, 175, 0
 
 bool rgb_matrix_indicators_user() {
     // if (layer_state_is(_FN1)) {
