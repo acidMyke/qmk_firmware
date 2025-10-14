b44fc4c7523cc629e6854217a753038d3b5dc86a
acidMyke <owner@acidmyke.link>
2025-10-14 22:05:39 +0800

    feat: move keymaps from v8 to v8_max

---
+++ keyboards/keychron/v8_max/ansi_encoder/keymaps/acidmyke/colors.h
@@ -0,0 +1,69 @@
+// #define DIM_RGB_AZURE       153-80, 245-80, 255-80
+// #define DIM_RGB_BLUE        0, 0, 255-80
+// #define DIM_RGB_CHARTREUSE  128-80, 255-80, 0
+// #define DIM_RGB_CORAL       255-80, 124-80, 77-80
+// #define DIM_RGB_CYAN        0, 255-80, 255-80
+// #define DIM_RGB_GOLD        255-80, 217-80, 0
+// #define DIM_RGB_GOLDENROD   217-80, 165-80, 33-80
+// #define DIM_RGB_GREEN       0, 255-80, 0
+// #define DIM_RGB_MAGENTA     255-80, 0, 255-80
+// #define DIM_RGB_ORANGE      255-80, 128-80, 0
+// #define DIM_RGB_PINK        255-80, 128-80, 191-80
+// #define DIM_RGB_PURPLE      122-80, 0, 255-80
+// #define DIM_RGB_RED         255-80, 0, 0
+// #define DIM_RGB_SPRINGGREEN 0, 255-80, 128-80
+// #define DIM_RGB_TEAL        0, 128-80, 128-80
+// #define DIM_RGB_TURQUOISE   71-80, 110-80, 106-80
+// #define DIM_RGB_WHITE       255-80, 255-80, 255-80
+// #define DIM_RGB_YELLOW      255-80, 255-80, 0
+
+#define DIM_RGB_AZURE       73, 165, 175
+#define DIM_RGB_BLACK       0, 0, 0
+#define DIM_RGB_BLUE        0, 0, 175
+#define DIM_RGB_CHARTREUSE  48, 175, 0
+#define DIM_RGB_CORAL       175, 44, 0
+#define DIM_RGB_CYAN        0, 175, 175
+#define DIM_RGB_GOLD        175, 137, 0
+#define DIM_RGB_GOLDENROD   137, 85, 0
+#define DIM_RGB_GREEN       0, 175, 0
+#define DIM_RGB_MAGENTA     175, 0, 175
+#define DIM_RGB_ORANGE      175, 48, 0
+#define DIM_RGB_PINK        175, 38, 95
+#define DIM_RGB_PURPLE      42, 0, 175
+#define DIM_RGB_RED         175, 0, 0
+#define DIM_RGB_SPRINGGREEN 0, 175, 48
+#define DIM_RGB_TEAL        0, 48, 48
+#define DIM_RGB_TURQUOISE   0, 30, 26
+#define DIM_RGB_WHITE       175, 175, 175
+#define DIM_RGB_YELLOW      175, 175, 0
+
+#define DIM_HSV_AZURE        132, 102, 255
+#define DIM_HSV_BLACK          0,   0,   0
+#define DIM_HSV_BLUE         170, 255, 255
+#define DIM_HSV_CHARTREUSE    64, 255, 255
+#define DIM_HSV_CORAL         11, 176, 255
+#define DIM_HSV_CYAN        128, 255, 255
+#define DIM_HSV_GOLD         36, 255, 255
+#define DIM_HSV_GOLDENROD    30, 218, 218
+#define DIM_HSV_GREEN        85, 255, 255
+#define DIM_HSV_MAGENTA     213, 255, 255
+#define DIM_HSV_ORANGE       21, 255, 255
+#define DIM_HSV_PINK        234, 128, 255
+#define DIM_HSV_PURPLE      191, 255, 255
+#define DIM_HSV_RED          0, 255, 255
+#define DIM_HSV_SPRINGGREEN 106, 255, 255
+#define DIM_HSV_TEAL       128, 255, 128
+#define DIM_HSV_TURQUOISE  123,  90, 112
+#define DIM_HSV_WHITE        0,   0, 255
+#define DIM_HSV_YELLOW      43, 255, 255
+#define DIM_HSV_OFF       DIM_HSV_BLACK
+
+
+
+/* Keymap -> Lighting index
+    GRV:00,   1:01,   2:02,  3:03,   4:04,  5:05,   6:06,   7:07, 8:08,   9:09,   0:10,  MIN:11,   EQL:12,  BSPC:13,            MUTE:14,
+    TAB:15,   Q:16,   W:17,  E:18,   R:19,  T:20,   Y:21,   U:22, I:23,   O:24,   P:25,  LBR:26,  RBRC:27,  BSLS:28,            PSCR:29,
+    ESC:30,   A:31,   S:32,  D:33,   F:34,  G:35,           H:36, J:37,   K:38,   L:39,  SCL:40,  QUOT:41,   ENT:42,             DEL:43,
+    SFT:44,           Z:45,  X:46,   C:47,  V:48,   B:49,   B:50, N:51,   M:52, COM:53,  DOT:54,  SLSH:55,  RSFT:56,    UP:57,
+    CTL:58, WIN:59, ALT:60,        SPC:61,        FN1:62, FN2:63,       SPC:64,          ALT:65,            LEFT:66,  DOWN:67,  RGHT:68
+*/
---
+++ keyboards/keychron/v8_max/ansi_encoder/keymaps/acidmyke/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define RGBLIGHT_DEFAULT_HUE 179 // Sets the default hue value, if none has been set
+#define RGBLIGHT_DEFAULT_SAT 224 // Sets the default saturation value, if none has been set
+#define RGBLIGHT_DEFAULT_VAL 145
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 170
+#define RGBLIGHT_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#define RGBLIGHT_SLEEP // turn off effects when suspended
+#define RGBLIGHT_LAYERS
+// #define HOLD_ON_OTHER_KEY_PRESS
+// #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
