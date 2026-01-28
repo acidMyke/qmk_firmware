f824c0adbf2b55cadd2a98a03db0eaad1aa4da83
lokher <lokher@gmail.com>
2026-01-28 17:45:18 +0800

    Add per-key/mixed RGB to Keychron Q0

--- keyboards/keychron/q0/keycodes_custom.h
+++ keyboards/keychron/q0/keycodes_custom.h
@@ -1,4 +1,4 @@
-/* Copyright 2023 ~ 2025 @ Keychron (https://www.keychron.com)
+/* Copyright 2025 @ Keychron (https://www.keychron.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,9 +14,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "keychron.h"
+#pragma once
 
-void keyboard_post_init_kb(void) {
-    keychron_common_init();
-    keyboard_post_init_user();
-}
+#include "keycodes.h"
+
+enum custom_keycodes {
+    KC_LOPTN = QK_KB_2,
+    KC_ROPTN,
+    KC_LCMMD,
+    KC_RCMMD,
+    KC_MAC_SIRI,
+    KC_WIN_TASK_VIEW,
+    KC_WIN_FILE_EXPLORER,
+    KC_MAC_SCREEN_SHOT,
+    KC_WIN_CORTANA,
+    NEW_SAFE_RANGE,
+};
+
+#define KC_SIRI KC_MAC_SIRI
+#define KC_TASK KC_WIN_TASK_VIEW
+#define KC_FILE KC_WIN_FILE_EXPLORER
+#define KC_SNAP KC_MAC_SCREEN_SHOT
+#define KC_CTANA KC_WIN_CORTANA
