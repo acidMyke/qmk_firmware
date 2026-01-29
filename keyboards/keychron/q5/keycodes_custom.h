35ebdc1f98ad408b75ebb295d450efe67b9c9a76
lokher <lokher@gmail.com>
2026-01-29 12:19:46 +0800

    Add per-key/mixed RGB to Keychron Q5

--- keyboards/keychron/q5/keycodes_custom.h
+++ keyboards/keychron/q5/keycodes_custom.h
@@ -1,4 +1,4 @@
-/* Copyright 2023 ~ 2025 @ Keychron (https://www.keychron.com)
+/* Copyright 2025 @ Keychron (https://www.keychron.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,23 @@
 
 #pragma once
 
-/* Indications */
-#define CAPS_LOCK_INDEX 50
-#define NUM_LOCK_INDEX 30
+#include "keycodes.h"
+
+enum custom_keycodes {
+    KC_LOPTN = QK_KB_2,
+    KC_ROPTN,
+    KC_LCMMD,
+    KC_RCMMD,
+    KC_MAC_SIRI,
+    KC_WIN_TASK_VIEW,
+    KC_WIN_FILE_EXPLORER,
+    KC_MAC_SCREEN_SHOT,
+    KC_WIN_CORTANA,
+    NEW_SAFE_RANGE,
+};
+
+#define KC_SIRI KC_MAC_SIRI
+#define KC_TASK KC_WIN_TASK_VIEW
+#define KC_FILE KC_WIN_FILE_EXPLORER
+#define KC_SNAP KC_MAC_SCREEN_SHOT
+#define KC_CTANA KC_WIN_CORTANA
