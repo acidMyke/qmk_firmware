cb228e28937c434e9d98a1737a28e79ac1ef349d
acidMyke <owner@acidmyke.link>
2026-05-01 15:15:51 +0800

    update defaults to use RGB_MATRIX instead of RGBLIGHT

--- keyboards/keychron/v8_max/ansi_encoder/keymaps/acidmyke/config.h
+++ keyboards/keychron/v8_max/ansi_encoder/keymaps/acidmyke/config.h
@@ -3,12 +3,16 @@
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0 // Disables tap-and-hold repeat for mod-taps
-#define RGBLIGHT_DEFAULT_HUE 179 // Sets the default hue value, if none has been set
-#define RGBLIGHT_DEFAULT_SAT 224 // Sets the default saturation value, if none has been set
-#define RGBLIGHT_DEFAULT_VAL 145
+#define RGB_MATRIX_DEFAULT_HUE 179 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 224 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_VAL 145
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 170
-#define RGBLIGHT_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
-#define RGBLIGHT_SLEEP // turn off effects when suspended
+#define RGB_MATRIX_HUE_STEP 4 // The value by which to increment the hue per adjustment action
+#define RGB_MATRIX_SAT_STEP 8 // The value by which to increment the saturation per adjustment action
+#define RGB_MATRIX_VAL_STEP 8 // The value by which to increment the brightness per adjustment action
+#define RGB_MATRIX_SPD_STEP 8 // The value by which to increment the animation speed per adjustment action
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_SLEEP // turn off effects when suspended
 #define RGBLIGHT_LAYERS
 // #define HOLD_ON_OTHER_KEY_PRESS
 // #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
