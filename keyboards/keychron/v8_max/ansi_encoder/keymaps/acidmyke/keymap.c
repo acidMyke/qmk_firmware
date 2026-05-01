210fc6454a2d52db23b16cdf35b424ea85111c43
acidMyke <owner@acidmyke.link>
2026-05-01 14:32:00 +0800

    numpad mode

--- keyboards/keychron/v8_max/ansi_encoder/keymaps/acidmyke/keymap.c
+++ keyboards/keychron/v8_max/ansi_encoder/keymaps/acidmyke/keymap.c
@@ -44,7 +44,8 @@ enum layers{
     __MT,
     _FN1,
     _FN2,
-    _FN3
+    _FN3,
+    __NP
 };
 
 // Taps (Mod Tap & Layer Tap)
@@ -131,35 +132,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,   KC_T, KC_Y,   KC_U,     KC_I, KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PSCR,
         KC_ESC,  KC_A,    KC_S,    KC_D, KC_F,   KC_G,         KC_H,     KC_J, KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_DEL,
         KC_LSFT,          KC_Z,    KC_X, KC_C,   KC_V, KC_B,   KC_B,     KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-        KC_LCTL, KC_LWIN, KC_LALT,       KC_SPC,       LT_FN1, LT_FN2,       KC_SPC,          KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN, KC_LALT,       KC_SPC,       LT_FN1, LT_FN2,       KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
     // Home row mods enabled
     [__MT] = LAYOUT_ansi_69(
         KC_GRV,  KC_1,    KC_2,    KC_3, KC_4,    KC_5, KC_6,   KC_7,   KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,    KC_T, KC_Y,   KC_U,   KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PSCR,
         MT_ESC,  KC_A,    MT_S,    MT_D, MT_F,    MT_G,         MT_H,   MT_J, MT_K,    KC_L,    MT_SCLN, KC_QUOT, KC_ENT,           KC_DEL,
         KC_LSFT,          KC_Z,    KC_X, KC_C,    KC_V, LT_B,   LT_B,   KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-        _______, _______, KC_LALT,       LT_LSPC,       LT_FN1, LT_FN2,       LT_RSPC,          _______,          KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN, KC_LALT,       LT_LSPC,       LT_FN1, LT_FN2,       LT_RSPC,          KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT),
     // Function layer 1 (VIM Nav & F1-12 keys)
     [_FN1] = LAYOUT_ansi_69(
-        KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          KC_MPLY,
-        KC_TAB,  _______,  KC_UP,   _______, _______, _______,  C(KC_Y),  KC_UP,   _______, _______, _______,  _______,  _______,  KC_PLUS,          KC_INS,
-        MT_ESC,  KC_LEFT,  KC_DOWN, KC_RGHT, MT_F,    MT_G,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______,  _______,  KC_EQL,           _______,
-        _______,           C(KC_Z), C(KC_X), C(KC_C), C(KC_V),  C(KC_B),  _______, _______, _______, KC_DOWN,  _______,  _______,  _______, KC_PGUP,
-        _______, _______,  KC_LALT,          LT_LSPC,           LT_FN1,   LT_FN2,           LT_RSPC,           _______,            KC_HOME, KC_PGDN,  KC_END),
+        KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   _______,           KC_MPLY,
+        KC_TAB,  _______,  KC_UP,   _______, _______, _______,  C(KC_Y),  KC_UP,   _______, _______, _______, _______,  _______,  KC_PLUS,           KC_INS,
+        MT_ESC,  KC_LEFT,  KC_DOWN, KC_RGHT, MT_F,    MT_G,               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,  _______,  KC_EQL,            _______,
+        KC_LSFT,           C(KC_Z), C(KC_X), C(KC_C), C(KC_V),  C(KC_B),  _______, _______, _______, KC_DOWN, _______,  _______,  _______, KC_PGUP,
+        KC_LCTL, KC_LWIN,  KC_LALT,          LT_LSPC,           LT_FN1,   LT_FN2,           LT_RSPC,          _______,            KC_HOME, KC_PGDN,  KC_END),
     // Function layer 2 (RTL)
     [_FN2] = LAYOUT_ansi_69(
-        KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,    KC_F5,      KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   _______,          C(A(KC_F22)),
-        KC_TAB,  _______,  KC_PGUP, _______, _______,  _______,    C(KC_Y),  KC_PGUP, _______, _______, _______, _______,  _______,  _______,          _______,
-        MT_ESC,  KC_HOME,  KC_PGDN, KC_END,  MT_F,     MT_G,       KC_HOME,  KC_PGDN, KC_PGUP, KC_END,  _______, _______,  _______,          _______,
-        _______,           C(KC_Z), C(KC_X), C(KC_T),  C(S(KC_O)), C(KC_P),  _______, _______, _______, KC_PGDN, _______,  _______,  _______,  KC_PGUP,
-        _______, _______,  KC_LALT,          LT_LSPC,              LT_FN1,   LT_FN2,           LT_RSPC,          _______,            KC_HOME, KC_PGDN, KC_END),
+        KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,    KC_F5,      KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          C(A(KC_F22)),
+        KC_TAB,  _______,  KC_PGUP, _______, _______,  _______,    C(KC_Y),  KC_PGUP, _______, _______, _______, _______, _______, _______,          _______,
+        MT_ESC,  KC_HOME,  KC_PGDN, KC_END,  MT_F,     MT_G,                 KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______,          _______,
+        _______,           C(KC_Z), C(KC_X), C(KC_T),  C(S(KC_O)), C(KC_P),  _______, _______, _______, KC_PGDN, _______, _______, _______, KC_PGUP,
+        _______, _______,  KC_LALT,          LT_LSPC,              LT_FN1,   LT_FN2,           LT_RSPC,          _______,          KC_HOME, KC_PGDN, KC_END),
     // Function layer 3 (F13-24 keys)
     [_FN3] = LAYOUT_ansi_69(
         KC_TILD, SS_PASS_1, SS_PASS_2, SS_PASS_3, SS_PASS_4, SS_PASS_5, SS_PASS_6, SS_PASS_7, SS_PASS_8, SS_PASS_9, SS_PASS_0, RM_SPDD,  RM_SPDU,  _______,          UG_TOGG,
-        RM_ON,   RM_NEXT,   RM_HUEU,   RM_SATU,   RM_VALU,   RM_SPDU,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,          _______,
-        RM_OFF,  RM_PREV,   RM_HUED,   RM_SATD,   RM_VALD,   RM_SPDD,              _______,   _______,   _______,   _______,   _______,  _______,  _______,          _______,
-        _______,            _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______, _______,
-        _______, _______,   KC_LALT,              _______,              _______,   _______,              _______,              _______,            _______, _______, _______)
+        _______, BT_HST1,   BT_HST2,   BT_HST3,   P2P4G,     _______,   RM_NEXT,   KC_P4,     KC_P5,     KC_P6,     KC_PPLS,   _______,  _______,  _______,          _______,
+        RM_ON,   RM_NEXT,   RM_HUEU,   RM_SATU,   RM_VALU,   RM_SPDU,              _______,   KC_P1,     KC_P2,     KC_P3,     KC_PENT,  _______,  _______,          _______,
+        RM_OFF,             RM_PREV,   RM_HUED,   RM_SATD,   RM_VALD,   RM_SPDD,   _______,   TG(__NP),  KC_P0,     KC_COMM,   KC_DOT,   _______,  _______, _______,
+        _______, _______,   KC_LALT,              _______,              _______,   _______,              KC_P0,                _______,            _______, _______, _______),
+    [__NP] = LAYOUT_ansi_69(
+        _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS,  KC_PPLS,  KC_BSPC,          _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,  _______,  _______,          _______,
+        _______, _______, _______, _______, _______, _______,          _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT,  _______,  KC_PENT,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PCMM, KC_PDOT,  _______,   _______, _______,
+        _______, _______, _______,          _______,          _______, _______,          KC_P0,             _______,            _______, _______, _______)
     // [TEMPLATE] = LAYOUT_ansi_69(
     //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
     //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
@@ -170,13 +177,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [__NMT] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [__MT] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [__NMT]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [__MT]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_FN1]   = {ENCODER_CCW_CW(KC_MPRV, KC_MNXT)},
     [_FN2]   = {ENCODER_CCW_CW(C(S(KC_F22)), C(KC_F22))},
-    [_FN3]   = {ENCODER_CCW_CW(RM_VALD, RM_VALU)}
+    [_FN3]   = {ENCODER_CCW_CW(RM_VALD, RM_VALU)},
+    [__NP]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif // ENCODER_MAP_ENABLE
+uint8_t num_block_rgb[] = {7, 8, 9, 22, 23, 24, 25, 36, 37, 38, 39, 51, 52};
 
 bool rgb_matrix_indicators_user() {
     uint8_t layer = biton32(default_layer_state);
@@ -209,6 +218,12 @@ bool rgb_matrix_indicators_user() {
         rgb_matrix_set_color(48, DIM_RGB_MAGENTA); // V
         rgb_matrix_set_color(49, DIM_RGB_MAGENTA); // B 
     }
+    
+    if (layer_state_is(__NP) || layer == __NP || layer_state_is(_FN3) || layer == _FN3) {
+        for (int i = 0; i < 13; i++) {
+            rgb_matrix_set_color(num_block_rgb[i], DIM_RGB_GOLD);
+        }
+    }
 
     if (layer >= __NMT && layer <= _FN3) {
         rgb_matrix_set_color(layer, DIM_RGB_TURQUOISE);
@@ -221,6 +236,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     if (!process_record_keychron_common(keycode, record)) {
         return false;
+    }
+
+    if (current_layer == __NP && record->event.pressed) {
+        switch (keycode) {
+            // Keys that are ALLOWED to stay on the Numpad layer
+            case KC_P1 ... KC_P0:
+            case KC_PDOT:
+            case KC_PCMM:
+            case KC_PMNS:
+            case KC_PPLS:
+            case KC_PENT:
+            case KC_BSPC:
+                return true; // Process normally
+            
+            // Any other key will kick you back to the main layer
+            default:
+                layer_off(__NP);
+                layer_on(__MT);
+                return true; 
+        }
     }
 
     if (!process_record_password(keycode, record)) {
